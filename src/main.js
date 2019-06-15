82f8df4880df4de15806b741c98cc33c4cfd9a44
jepiqueau <jepi.queau@free.fr>
2019-06-15 12:48:33 +0200

    fix toast issue on web by adding pwa-elements

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Ionic from "@ionic/vue";
+import { defineCustomElements as pwaElements } from "@ionic/pwa-elements/loader";
 
 Vue.use(Ionic);
+pwaElements(window);
 
 new Vue({
   router,
